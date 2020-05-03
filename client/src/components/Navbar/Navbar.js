bd9211a3db975e66ece882406f795c74f638615d
cheyennekim <cheyenne.kim@duke.edu>
2020-05-03 03:24:12 -0400

    hhojioio

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,20 +10,12 @@ class NavBar extends Component {
     }
     render() {
         return(
-            <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar expand="lg" style={{marginBottom:"-20px"}}>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      <Nav.Link href="#home" style ={{fontSize:"20px",color:"black",marginRight:"20px",marginLeft:"50px",marginTop:"20px"}}>Menu</Nav.Link>
+      <Nav.Link href="#link" style ={{fontSize:"20px",color:"black",marginRight:"20px",marginLeft:"20px",marginTop:"20px"}}>Order Online</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>

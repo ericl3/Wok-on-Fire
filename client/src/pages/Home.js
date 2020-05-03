54baf14db64bb0391c51fddededde57909ef62c2
cheyennekim <cheyenne.kim@duke.edu>
2020-05-03 02:09:17 -0400

    asdf

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,11 +4,17 @@ import { Container, Row, Col, Button } from 'react-bootstrap'
 
 class Home extends Component {
     render() {
+        const mystyle = {
+            width: "50%",
+            height: "50%"
+          };
         return (
             <Container>
                 <Row>
                     <Col>
                         <center><img src={require("../images/logo.png")}/></center>
+                        <br></br>
+                        <center><img src={require("../images/homepage graphic.png")} style={mystyle}/></center>
                     </Col>
                 </Row>
             </Container>

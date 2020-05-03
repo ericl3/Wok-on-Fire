060111a3c7b6cc7ecd766f925bc6666ba07f40ca
Eric <el172@duke.edu>
2020-05-03 02:29:40 -0400

    added border

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Table } from 'react-bootstrap'
 
 class Home extends Component {
     render() {
         const mystyle = {
-            width: "50%",
-            height: "50%"
+            width: "45vw",
+            height: "75vh"
           };
+        const containerStyle = {
+            border: "solid 4px red",
+            height: "99vh"
+        }
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <center><img src={require("../images/logo.png")}/></center>
-                        <br></br>
-                        <center><img src={require("../images/homepage graphic.png")} style={mystyle}/></center>
-                    </Col>
-                </Row>
-            </Container>
+            <body>
+                <Container fluid style={containerStyle}>
+                    <Row>
+                        <Col>
+                            <center><img src={require("../images/logo.png")}/></center>
+                            <br></br>
+                            <center><img src={require("../images/homepage graphic.png")} style={mystyle}/></center>
+                        </Col>
+                    </Row>
+                </Container>
+            </body>
         )
     }
 }
